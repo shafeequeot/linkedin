ace96b90f8d381a9ae25d5b253ba7d4c8a22b815
Shafeeque OT <shafeequeot@gmail.com>
2022-05-06 01:27:03 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <h1>let build linked in</h1>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBmk-BGI8z_VqCxq13vWL9nfngPm7nnyuo",
+//   authDomain: "linkedin-6222a.firebaseapp.com",
+//   projectId: "linkedin-6222a",
+//   storageBucket: "linkedin-6222a.appspot.com",
+//   messagingSenderId: "993214341426",
+//   appId: "1:993214341426:web:5b63863fb617446156772b"
+// };
